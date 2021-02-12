35cc06947cc3b70e066841e9b5dc886f169952ec
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-12 08:24:48 +0200

    lights

--- pages/admin/lights.vue
+++ pages/admin/lights.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class=" container_hg admin_container">
+  <div class="container_hg admin_container">
     <RightSide />
     <light />
-
-   </div>
+  </div>
 </template>
 
 <script>
-   export default {
-      layout: 'admin'
-   }
+export default {
+  layout: "admin",
+};
 </script>
 
 <style lang="scss" scoped>
-.admin_container{
-   display: grid;
-   display: -moz-grid;
-   display: -ms-grid;
-   grid-template-columns: 250px auto;
-   @media screen and(max-width: 768px){
-      grid-template-columns: none;
-      display: flex;
-      display: -webkit-flex;
-      display: -ms-flexbox;
-      flex-direction: column;
-      -ms-flex-direction: row;
-      flex-wrap: nowrap;
-      -ms-flex-wrap: nowrap;
-   }
+.admin_container {
+  display: grid;
+  display: -moz-grid;
+  display: -ms-grid;
+  grid-template-columns: 250px auto;
+  @media screen and(max-width: 768px) {
+    grid-template-columns: none;
+    display: flex;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    flex-direction: column;
+    -ms-flex-direction: row;
+    flex-wrap: nowrap;
+    -ms-flex-wrap: nowrap;
+  }
 }
 </style>
