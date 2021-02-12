2684f2d59389f7a051bc52a023b8b17704403f0e
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-12 08:25:15 +0200

    setNewArticle() in articles store

--- store/articles.js
+++ store/articles.js
@@ -1,6 +1,7 @@
 
 export const state = () => ({
-  articles: []
+  articles: [],
+  newArticle: null
 });
 export const getters = {
   articles: (state) => state.articles,
@@ -8,9 +9,16 @@ export const getters = {
 
 export const mutations = {
   setArticles(state, articles) { state.articles = articles },
+  setNewArticle(state, article) {
+    state.newArticle = article;
+    state.articles = [article, ...state.articles];
+  },
 };
 export const actions = {
   setArticles({ commit }, articles) {
     commit("setArticles", articles);
+  },
+  setNewArticle({ commit }, article) {
+    commit("setNewArticle", article);
   },
 };
