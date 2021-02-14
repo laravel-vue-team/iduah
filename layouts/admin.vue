51fad5c3c82d960656943ee56c9b924e1336cec7
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-14 13:43:34 +0200

    admin layout

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -4,3 +4,17 @@
     <Nuxt />
   </div>
 </template>
+<script>
+export default {
+  middleware: "authenticated",
+  mounted() {
+    if (process.client) {
+      this.$store.dispatch("auth/setToken", localStorage.getItem("TOKEN"));
+      this.$store.dispatch(
+        "auth/setUser",
+        JSON.parse(localStorage.getItem("USER"))
+      );
+    }
+  },
+};
+</script>
