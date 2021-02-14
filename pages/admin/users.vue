e87535c5474654fe96b294d934651100ac8e4e62
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-14 13:46:01 +0200

    users

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -2,31 +2,30 @@
   <div class="container_hg admin_container">
     <RightSide />
     <users />
-
-   </div>
+  </div>
 </template>
 
 <script>
-   export default {
-      layout: 'admin'
-   }
+export default {
+  layout: "admin",
+};
 </script>
 
 <style lang="scss" scoped>
-.admin_container{
-   display: grid;
-   display: -moz-grid;
-   display: -ms-grid;
-   grid-template-columns: 250px auto;
-   @media screen and(max-width: 768px){
-      grid-template-columns: none;
-      display: flex;
-      display: -webkit-flex;
-      display: -ms-flexbox;
-      flex-direction: column;
-      -ms-flex-direction: row;
-      flex-wrap: nowrap;
-      -ms-flex-wrap: nowrap;
-   }
+.admin_container {
+  display: grid;
+  display: -moz-grid;
+  display: -ms-grid;
+  grid-template-columns: 250px auto;
+  @media screen and(max-width: 768px) {
+    grid-template-columns: none;
+    display: flex;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    flex-direction: column;
+    -ms-flex-direction: row;
+    flex-wrap: nowrap;
+    -ms-flex-wrap: nowrap;
+  }
 }
 </style>
