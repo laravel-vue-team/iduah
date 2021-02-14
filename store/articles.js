ec8383c247327aafb69e32691cac5ac693ba596e
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-14 13:46:16 +0200

    articles store

--- store/articles.js
+++ store/articles.js
@@ -1,7 +1,9 @@
 
 export const state = () => ({
   articles: [],
-  newArticle: null
+  newArticle: null,
+  currentPage: null,
+  isThereNextPage: null,
 });
 export const getters = {
   articles: (state) => state.articles,
@@ -9,16 +11,42 @@ export const getters = {
 
 export const mutations = {
   setArticles(state, articles) { state.articles = articles },
+  setCurrentPage(state, currentPage) { state.currentPage = currentPage },
+  setIsThereNextPage(state, isThereNextPage) { state.isThereNextPage = isThereNextPage },
   setNewArticle(state, article) {
     state.newArticle = article;
     state.articles = [article, ...state.articles];
   },
 };
 export const actions = {
+  fetchArticles({ commit }) {
+    this.$axios
+      .get("/api/posts")
+      .then((res) => {
+        commit("setArticles", [...res.data.data.data]);
+        commit("setCurrentPage", res.data.data.current_page);
+        commit("setIsThereNextPage", res.data.data.next_page_url ? true : false);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  },
   setArticles({ commit }, articles) {
     commit("setArticles", articles);
   },
   setNewArticle({ commit }, article) {
     commit("setNewArticle", article);
   },
+  async deleteArticle({ commit, state }, obj) {
+    try {
+      let res = await this.$axios.delete(`/api/posts/${obj.id}/delete`);
+      console.log(res.data);
+      let arr = [...state.articles];
+      arr.splice(obj.index, 1);
+      commit("setArticles", arr);
+    } catch (e) {
+      console.log(e.response);
+    }
+
+  }
 };
