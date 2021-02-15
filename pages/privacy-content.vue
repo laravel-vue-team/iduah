16cf1d98ffe0cfbaf1ed5ac6a352b4b345f2f8b5
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-15 07:56:47 +0200

    privacy content

--- pages/privacy-content.vue
+++ pages/privacy-content.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="container_responsive_wrapper container_hg">
-
-   </div>
+    <div class="container">
+      <main>
+        <h1 class="head_txt">سياسات الخصوصية:</h1>
+        <div class="txt" v-html="privacy"></div>
+      </main>
+    </div>
+  </div>
 </template>
 
 <script>
-   export default {
-      
-   }
+export default {
+  created() {
+    this.$store.dispatch("textEditor/fetchSettings");
+  },
+  computed: {
+    privacy() {
+      console.log(this.$store.getters["textEditor/privacy"]);
+      return this.$store.getters["textEditor/privacy"];
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+  margin: 5vh 5vw;
+  height: 81vh;
+  background-color: rgb(201, 201, 201, 0.3);
+  padding: 10px 20px 10px;
+  border-radius: 20px 20px 8px 8px;
+  backdrop-filter: blur(5px);
+}
+.head_txt {
+  color: rgb(40, 40, 40);
+}
+.txt {
+  margin-top: 20px;
+}
 </style>
