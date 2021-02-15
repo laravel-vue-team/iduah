2c88c69067bf8e7f05e6d995d6b395d9f3789b26
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-15 08:22:19 +0200

    categories store

--- store/categories.js
+++ store/categories.js
@@ -1,4 +1,3 @@
-
 export const state = () => ({
   categories: [],
   newCategory: null,
@@ -9,6 +8,7 @@ export const getters = {
 
 export const mutations = {
   setCategories(state, categories) { state.categories = categories },
+  addCategory(state, category) { state.categories = [...state.categories, category] },
 };
 export const actions = {
   fetchCategories({ commit }) {
@@ -26,7 +26,36 @@ export const actions = {
   setCategories({ commit }, categories) {
     commit("setCategories", categories);
   },
-  // addCategory({ commit }, category) {
-  //   commit("addCategory", category);
-  // },
+  addCategory({ commit }, category) {
+    commit("addCategory", category);
+  },
+  deleteCategory({ commit, state }, obj) {
+    let config = {
+      method: "delete",
+      url: `http://idauh.com/idauh/api/categories/${obj.id}/delete`,
+    };
+    this.$axios(config)
+      .then((response) => {
+        let arr = [...state.categories];
+        arr.splice(obj.index, 1);
+        commit("setCategories", arr);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  editCategory({ commit, state }, obj) {
+    this.$axios.post(`/api/categories/${obj.id}/update`, { title: obj.title, status: 1 })
+      .then(res => {
+        let arr = [...state.categories];
+        console.log(arr);
+        arr[obj.index] = { ...arr[obj.index], title: obj.title };
+        commit("setCategories", arr);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err.response);
+      })
+  }
 };
