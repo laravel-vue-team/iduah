d477abe989ea9a7b8b1e8f975e9ee33e7c170646
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-18 22:48:35 +0200

    define private routes

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,10 +11,18 @@ export default {
     if (process.client) {
       this.$store.commit("auth/setIsLoading", false);
       this.$store.dispatch("auth/setToken", localStorage.getItem("TOKEN"));
+      console.log(this.$router.history._startLocation);
       if (
         !this.$store.getters["auth/isAuth"] &&
-        this.$router.history._startLocation !== "/" &&
-        this.$router.history._startLocation !== "/articles"
+        (this.$router.history._startLocation === "/profile" ||
+          this.$router.history._startLocation === "/admin" ||
+          this.$router.history._startLocation === "/admin/users" ||
+          this.$router.history._startLocation ===
+            "/admin/administrative_pages" ||
+          this.$router.history._startLocation === "/admin/sections" ||
+          this.$router.history._startLocation === "/admin/lights" ||
+          this.$router.history._startLocation === "/admin/articles" ||
+          this.$router.history._startLocation === "/settings")
       ) {
         return this.$router.push("/login");
       }
