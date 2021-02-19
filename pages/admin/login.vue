f25a1e17d462d0c2f6d6ca3d5006116b1f1d29bd
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-19 22:11:07 +0200

    lint for login admin page

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,200 +1,231 @@
 <template>
-   <div class=" container_hg loging_container">
-      <div class="log_box">
-         <h3 class="title">تسجيل الدخول</h3>
+  <div class="container_hg loging_container">
+    <div class="log_box">
+      <h3 class="title">تسجيل الدخول</h3>
 
-         <p class="error_content transition" v-show="requires">
-            رجاءً تأكد من صحة البيانات الدخلة
-         </p>
+      <p class="error_content transition" v-show="requires">
+        رجاءً تأكد من صحة البيانات الدخلة
+      </p>
 
-         <form class="log-form" @submit.prevent="loginUser" ref="loging">
-            <div class="form_group">
-            <label for="email"> البريد الالكتروني <span class="required">*</span></label>
-               <input type="email" name="email" id="email" placeholder="example@example.com" autocomplete="off" autocorrect="off" spellcheck="off" required class="transition focus_elem">
-            </div>
-            <div class="form_group">
-               <label for="pass">كلمة السر <span class="required">*</span></label>
-               <input type="password" name="pass" id="pass" placeholder="••••••••" required autocorrect="off" autocomplete="off" spellcheck="off"  class="transition focus_elem">
-            </div>
+      <form class="log-form" @submit.prevent="loginUser" ref="loging">
+        <div class="form_group">
+          <label for="email">
+            البريد الالكتروني <span class="required">*</span></label
+          >
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="example@example.com"
+            autocomplete="off"
+            autocorrect="off"
+            spellcheck="off"
+            required
+            class="transition focus_elem"
+          />
+        </div>
+        <div class="form_group">
+          <label for="pass">كلمة السر <span class="required">*</span></label>
+          <input
+            type="password"
+            name="pass"
+            id="pass"
+            placeholder="••••••••"
+            required
+            autocorrect="off"
+            autocomplete="off"
+            spellcheck="off"
+            class="transition focus_elem"
+          />
+        </div>
 
-            <div class="form_group">
-               <label for="check" class="checkbox">
-               <input type="checkbox" name="rememeber" id="check" class="transition focus_elem">
-               تذكرني
-               </label>
-            </div>
-            <div class="form_group">
-               <button class="btn_submit trasistion"  name="submit" type="submit" @click="CheckRequires">دخول</button>
-            </div>
-         </form>
-      </div>
-   </div>
+        <div class="form_group">
+          <label for="check" class="checkbox">
+            <input
+              type="checkbox"
+              name="rememeber"
+              id="check"
+              class="transition focus_elem"
+            />
+            تذكرني
+          </label>
+        </div>
+        <div class="form_group">
+          <button
+            class="btn_submit trasistion"
+            name="submit"
+            type="submit"
+            @click="CheckRequires"
+          >
+            دخول
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-   export default {
-      layout: 'admin',
-      data() {
-         return {
-            requires: false,
-            title: 'تسجيل الدخول',
-            desc: 'قم بتسجيل الدخول الان واستكمل مهمتك في ادارة موقعك'
-         }
-      },
-      methods: {
-         loginUser(){
-            const CurrentSubmitForm = this.$refs.loging;
+export default {
+  layout: "admin",
+  data() {
+    return {
+      requires: false,
+      title: "تسجيل الدخول",
+      desc: "قم بتسجيل الدخول الان واستكمل مهمتك في ادارة موقعك",
+    };
+  },
+  methods: {
+    loginUser() {
+      const CurrentSubmitForm = this.$refs.loging;
 
-            const userinfo = {
-               email: CurrentSubmitForm['email'].value,
-               pass: CurrentSubmitForm['pass'].value
-            }
-            this.$router.push('/profile')
-         },
-         CheckRequires(){
-            const CurrentForm = this.$refs.loging;
-            const requires = CurrentForm.querySelectorAll('[required]');
-            
-            requires.forEach(require =>{
-               let val = require.value;
-               if (val === ''){
-                 return  this.requires = true,
-                  require.classList.add('empty_elem');
-               }else{
-                  return this.requires = false,
-                  require.classList.remove('empty_elem');
-                  
-               }
-            });
-         },
-         },
-       //seo tags to this page Home page
-       head() {
-      return {
-        title: this.title,
-        meta: [
-          {
-            hid: 'description',
-            name: 'description',
-            content: this.desc
-          },
-        ]
-      }
+      const userinfo = {
+        email: CurrentSubmitForm["email"].value,
+        pass: CurrentSubmitForm["pass"].value,
+      };
+      this.$router.push("/profile");
     },
-    name: 'login'
-   }
+    CheckRequires() {
+      const CurrentForm = this.$refs.loging;
+      const requires = CurrentForm.querySelectorAll("[required]");
+
+      requires.forEach((require) => {
+        let val = require.value;
+        if (val === "") {
+          return (this.requires = true), require.classList.add("empty_elem");
+        } else {
+          return (
+            (this.requires = false), require.classList.remove("empty_elem")
+          );
+        }
+      });
+    },
+  },
+  //seo tags to this page Home page
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.desc,
+        },
+      ],
+    };
+  },
+  name: "login",
+};
 </script>
 
 <style lang="scss" scoped>
-.loging_container{
-   position: relative;
-   padding: 20px 10px;
-   .log_box{
-      padding: 10px;
-      border-radius: 10px;
+.loging_container {
+  position: relative;
+  padding: 20px 10px;
+  .log_box {
+    padding: 10px;
+    border-radius: 10px;
+    position: relative;
+    display: block;
+    margin: auto;
+    max-width: 600px;
+    height: auto;
+    background-color: #fff;
+    box-shadow: 0 0 30px #f3f3f3;
+    .title {
+      text-align: center;
+      text-align: -webkit-center;
+      font-weight: 400;
+      padding: 5px 10px;
+      margin-bottom: 10px;
+    }
+    .error_content {
+      color: #eb596e;
+      padding: 0 10px 10px 5px;
+      text-align: center;
+      text-align: -webkit-center;
+      font-size: 17px;
+    }
+    .log-form {
+      width: 100%;
       position: relative;
-      display: block;
-      margin: auto;
-      max-width: 600px;
-      height: auto;
-      background-color: #fff;
-      box-shadow: 0 0 30px #f3f3f3;
-      .title{
-         text-align: center;
-         text-align: -webkit-center;
-         font-weight: 400;
-         padding: 5px 10px;
-         margin-bottom: 10px;
+      .form_group {
+        display: block;
+        margin-bottom: 10px;
+        width: 100%;
+        input {
+          height: 40px;
+        }
+        .checkbox {
+          input {
+            height: auto;
+            position: relative;
+            appearance: none;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            border-radius: 5px;
+            border: 1.5px solid #ddd;
+            width: 15px;
+            height: 15px;
+            padding: 0;
+            display: inline-block;
+            margin-left: 5px;
+            &:checked {
+              border: 5px solid #3176f7;
+              border-radius: 100px;
+            }
+          }
+        }
+        label {
+          display: block;
+          margin-bottom: 10px;
+          span {
+            color: #eb596e;
+            font-size: 20px;
+            font-weight: 600;
+          }
+        }
+        .focus_elem {
+          width: 100%;
+          padding: 10px 10px;
+          border-radius: 5px;
+          border: 1px solid #ddd;
+          background-color: #fff;
+          display: block;
+          &:focus {
+            box-shadow: 0 0 0 3px #bad2fd;
+            -webkit-box-shadow: 0 0 0 3px #d2e2ff;
+            border-color: #d2e2ff;
+          }
+        }
+        .empty_elem {
+          background-color: #eb596e50;
+          border-color: #eb596e60;
+          &:focus {
+            box-shadow: 0 0 0 3px #eb596e60;
+            -webkit-box-shadow: 0 0 0 3px #eb596e60;
+            border-color: #eb596e60;
+          }
+        }
+        a {
+          color: #1b5fdf;
+          text-decoration: none;
+        }
+        .btn_submit {
+          position: relative;
+          padding: 5px 15px;
+          background-color: #1b5fdf;
+          border-radius: 5px;
+          border: none;
+          color: #fff;
+          cursor: pointer;
+          &:disabled {
+            opacity: 0.5;
+            cursor: no-drop;
+          }
+        }
       }
-      .error_content{
-         color: #eb596e;
-         padding: 0 10px 10px 5px;
-         text-align: center;
-         text-align: -webkit-center;
-         font-size: 17px;
-      }
-      .log-form{
-         width: 100%;
-         position: relative;
-         .form_group{
-            display: block;
-            margin-bottom: 10px; 
-            width: 100%;
-            input{
-               height: 40px;
-            }
-            .checkbox{
-               input{
-                  height: auto;
-                  position: relative;
-                  appearance: none;
-                  -webkit-appearance: none;
-                  -moz-appearance: none;
-                  border-radius: 5px;
-                  border: 1.5px solid #ddd;
-                  width: 15px;
-                  height: 15px;
-                  padding: 0;
-                  display: inline-block;
-                  margin-left: 5px;
-                  &:checked{
-                     border: 5px solid #3176f7;
-                     border-radius: 100px;
-                  }
-               }
-            }
-            label{
-               display: block;
-               margin-bottom: 10px;
-               span{
-                  color: #eb596e;
-                  font-size: 20px;
-                  font-weight: 600;
-               }
-            }
-            .focus_elem{
-               width: 100%;
-               padding: 10px 10px;
-               border-radius: 5px;
-               border: 1px solid #ddd;
-               background-color: #fff;
-               display: block;
-               &:focus{
-                  box-shadow: 0 0 0 3px #bad2fd;
-                  -webkit-box-shadow: 0 0 0 3px #d2e2ff;
-                  border-color: #d2e2ff;
-
-               }
-            }
-            .empty_elem{
-               background-color: #eb596e50;
-               border-color: #eb596e60;
-                &:focus{
-                  box-shadow: 0 0 0 3px #eb596e60;
-                  -webkit-box-shadow: 0 0 0 3px #eb596e60;
-                  border-color: #eb596e60;
-
-               }
-            }
-           a{
-              color: #1b5fdf;
-              text-decoration: none;
-           }
-            .btn_submit{
-               position: relative;
-               padding: 5px 15px;
-               background-color: #1b5fdf;
-               border-radius: 5px;
-               border: none;
-               color: #fff;
-               cursor: pointer;
-               &:disabled{
-                  opacity: 0.5;
-                  cursor: no-drop;
-               }
-            }
-         }
-      }
-   }
+    }
+  }
 }
 </style>
