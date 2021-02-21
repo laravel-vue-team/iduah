1d37c3579046109db6444f33a6aa9139abfa33ba
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-22 01:57:59 +0200

    add summernote to adminstrive page

--- components/admin/AdministrativePages.vue
+++ components/admin/AdministrativePages.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
+  <div class="container_page">
     <div class="users_content">
       <table class="users_data" width="100%">
+        <div v-show="isLoading" class="spinner">
+          <spinner />
+        </div>
         <thead>
           <tr>
             <th>إسم الصفحة</th>
@@ -27,14 +30,14 @@
     <form
       class="edit_content"
       ref="EditContentPage"
-      @sumbit.prevent="formSubmit"
+      @sumbit.prevent=""
       v-show="show"
     >
       <client-only>
-        <TextEditor
+        <TextEditorCopy
           :pagepath="CurrentPagePath"
           :pagename="CurrentPageName"
-          v-on:show="toggleShow"
+          v-on:hide="toggleShow"
           v-on:save="formSubmit"
           v-on:change="handleChange"
           :initTxt="'s'"
@@ -46,6 +49,7 @@
 
 <script>
 import TextEditor from "~/components/admin/TextEditor";
+import Spinner from "../user/spinner.vue";
 export default {
   data() {
     return {
@@ -73,6 +77,7 @@ export default {
   },
   components: {
     TextEditor,
+    Spinner,
   },
   created() {
     this.$store.dispatch("textEditor/fetchSettings");
@@ -87,6 +92,9 @@ export default {
     privacy() {
       return this.$store.getters["textEditor/privacy"];
     },
+    isLoading() {
+      return this.$store.getters["textEditor/isLoading"];
+    },
   },
   methods: {
     toggleShow() {
@@ -97,11 +105,12 @@ export default {
       EditForm.setAttribute("data-index", id);
       this.show = true;
       this.currentHtml = "";
+      this.$store.commit("textEditor/setId", id + 2);
       this.$store.commit("textEditor/setId", id);
       this.CurrentPageName = this.pages[id].name;
       this.CurrentPagePath = this.pages[id].path;
     },
-    formSubmit() {
+    formSubmit(a) {
       const CurrentForm = this.$refs.EditContentPage;
       let itid = CurrentForm.dataset.index;
       let oldData = {
@@ -112,12 +121,12 @@ export default {
         terms: this.data.terms,
       };
       let newData =
-        itid === 0
-          ? { ...oldData, terms: "" + this.currentHtml }
-          : { ...oldData, privacy: "" + this.currentHtml };
-      itid === 0
-        ? this.$store.dispatch("textEditor/editTerms", newData)
-        : this.$store.dispatch("textEditor/editPrivacy", newData);
+        +itid !== 0
+          ? { ...oldData, privacy: "" + a }
+          : { ...oldData, terms: "" + a };
+      +itid !== 0
+        ? this.$store.dispatch("textEditor/editPrivacy", newData)
+        : this.$store.dispatch("textEditor/editTerms", newData);
     },
     handleChange(newContent) {
       this.currentHtml = newContent;
@@ -127,6 +136,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.spinner {
+  position: absolute;
+  margin: 2vh auto;
+  width: 100%;
+  z-index: 9999;
+}
+.container_page {
+  /* height: 100vh; */
+  /* overflow-y: scroll; */
+  &::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 4px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: rgba(0, 0, 0, 0.5);
+    box-shadow: 0 0 1px rgb(255 255 255 / 50%);
+  }
+}
 .users_content {
   padding: 0 5px 0 0;
   width: 100%;
