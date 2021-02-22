5bce3c9508f31ccefdab8c879728aafd7fb560a8
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-22 02:06:38 +0200

    add isLoading for textEditor store

--- store/textEditor.js
+++ store/textEditor.js
@@ -3,13 +3,15 @@ export const state = () => ({
   privacy: "",
   terms: "",
   data: {},
-  id: 1,
+  id: -1,
+  isLoading: true,
 });
 export const getters = {
   privacy: (state) => state.privacy,
   terms: (state) => state.terms,
   id: (state) => state.id,
   data: (state) => state.data,
+  isLoading: (state) => state.isLoading,
 };
 
 export const mutations = {
@@ -17,45 +19,39 @@ export const mutations = {
   setTerms(state, terms) { state.terms = terms },
   setId(state, id) { state.id = id },
   setData(state, data) { state.data = data },
+  setIsLoading(state, isLoading) { state.isLoading = isLoading },
 };
 export const actions = {
   fetchSettings({ commit }) {
+    commit("setIsLoading", true);
     this.$axios
       .get("/api/settings")
       .then((res) => {
-        console.log(res.data.data);
         commit("setPrivacy", res.data.data.privacy);
+        commit("setIsLoading", false);
         commit("setTerms", res.data.data.terms);
         commit("setData", res.data.data);
       })
       .catch((err) => {
-        console.log(err.response);
+        commit("setIsLoading", false);
       });
   },
   editPrivacy({ commit }, newData) {
     this.$axios
       .put("/api/settings/update", newData)
       .then((res) => {
-        console.log(res.data);
-        console.log("new privacy");
         commit("setPrivacy", res.data.data.privacy);
       })
       .catch((err) => {
-        console.log(err);
-        console.log(err.data);
       });
   },
   editTerms({ commit }, newData) {
     this.$axios
       .put("/api/settings/update", newData)
       .then((res) => {
-        console.log(res.data);
-        console.log("new terms");
         commit("setTerms", res.data.data.terms);
       })
       .catch((err) => {
-        console.log(err);
-        console.log(err.data);
       });
   }
 };
