e13f0f2fd5ca25c73df39e81078c11739626d670
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2021-02-22 02:04:35 +0200

    add isLoading for allUsers

--- store/allUsers.js
+++ store/allUsers.js
@@ -1,29 +1,34 @@
 export const state = () => ({
-  allUsers: {},
+  users: {},
   currentPage: null,
   isThereNextPage: null,
+  isTherePrevPage: null,
 })
 
 export const getters = {
-  allUsers: (state) => state.allUsers,
+  users: (state) => state.users,
   currentPage: (state) => state.currentPage,
   isThereNextPage: (state) => state.isThereNextPage,
+  isTherePrevPage: (state) => state.isTherePrevPage,
 }
 
 export const mutations = {
-  setAllUsers(state, allUsers) { state.allUsers = allUsers },
+  setUsers(state, users) { state.users = users },
   setCurrentPage(state, currentPage) { state.currentPage = currentPage },
   setIsThereNextPage(state, isThereNextPage) { state.isThereNextPage = isThereNextPage },
+  setIsTherePrevPage(state, isTherePrevPage) { state.isTherePrevPage = isTherePrevPage },
 }
 
 export const actions = {
   fetchAllUsers({ commit }) {
     this.$axios.get("/api/users")
       .then(res => {
-        commit("setAllUsers", res.data.data.data);
+        commit("setUsers", res.data.data.data);
+        commit("setCurrentPage", res.data.data.current_page);
+        commit("setIsThereNextPage", res.data.data.next_page_url ? true : false);
+        commit("setIsTherePrevPage", res.data.data.prev_page_url ? true : false);
       })
       .catch(err => {
-        console.log(err.response);
       })
   },
 }
